fef53a175ae092366bc9390daf187be5c12f6b03
Andrey Lutogin <lutogin@trueconf.ru>
2019-08-20 15:18:11 +0300

    Add 6, 7

---
+++ solutions/p7.py
@@ -0,0 +1,16 @@
+"""Выписав первые шесть простых чисел, получим 2, 3, 5, 7, 11 и 13. Очевидно, что 6-ое простое число - 13.
+Какое число является 10001-ым простым числом?"""
+
+
+def simple_in_number(limit):
+    result = []
+    import math
+
+    for num in range(2, limit + 1):
+        if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
+            result.append(num)
+
+    return result[-1]
+
+
+print(simple_in_number(1001))
