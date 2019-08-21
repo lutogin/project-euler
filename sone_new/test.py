1cf5e57433ca8dd79561ced3aa9d9a0a04c15d45
Andrey Lutogin <lutogin@trueconf.ru>
2019-08-21 12:12:33 -0700

    add new

---
+++ sone_new/test.py
@@ -0,0 +1,16 @@
+ALPHABET = list('abcdefghijklmnopqrstuvwxyz')
+
+
+def coun_alb(c_str: str) -> int:
+    """Посчитывает сумму прдложения по системе a-1, b-2, c-3(номер буквы в ахлфавите) и так далее."""
+    raw_str = c_str.lower().split()
+    raw_str = "".join(raw_str)
+
+    summary = 0
+    for l in raw_str:
+        summary += ALPHABET.index(l) + 1
+
+    return summary
+
+
+print(coun_alb('NEW YORK STOCK EXCHANGE INCORPORATED'))
