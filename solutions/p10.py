4fcb6460140b7cf595f72a3b0ec692fa82f0c83b
Andrey Lutogin <lutogin@trueconf.ru>
2019-08-22 11:36:03 -0700

    Add 10

---
+++ solutions/p10.py
@@ -0,0 +1,19 @@
+"""
+Сумма простых чисел меньше 10 равна 2 + 3 + 5 + 7 = 17.
+
+Найдите сумму всех простых чисел меньше двух миллионов.
+"""
+
+
+def simple_number_sum(limit: int = 2000000) -> int:
+    result = []
+    import math
+
+    for num in range(2, limit):
+        if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
+            result.append(num)
+
+    return sum(result)
+
+
+print(simple_number_sum(2000000))
